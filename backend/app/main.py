8c9cbfbd4c05bed8d7daae4037f49cfdb8ed2ebf
quinn <quinn.borines@erfinderstore.com>
2025-05-16 13:24:11 +0800

    Fix volume path

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from app.database import MongoDatabaseSession
-from app.users import users
-from app.oauth import oauth
-
-get_db = MongoDatabaseSession("sample_users")
+from .users import users
+from .oauth import oauth
 
 app = FastAPI()
 
